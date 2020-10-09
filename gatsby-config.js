b8ef19829ecf8348f1c31fded2f84d4c7806db67
Andrew Park <atpark333@gmail.com>
2020-10-09 17:35:51 +0900

    Remove react-svg config

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,14 +29,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-plugin-react-svg`,
-      options: {
-	rule: {
-	  include: /\.inline\.svg$/,
-	},
-      }
-    }
+    `gatsby-plugin-react-svg`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
