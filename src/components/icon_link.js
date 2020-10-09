d5c8d2ef656137c86e8e48386e1fdc9108371160
Andrew Park <atpark333@gmail.com>
2020-10-09 11:10:13 +0900

    Code style change

--- src/components/icon_link.js
+++ src/components/icon_link.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const IconLink = (props) => {
   return (
-    <a href={props.uri} style={{color: 'white', display: 'inline-block', marginRight: '1.5em', ...props.style}} target="blank">
+    <a href={props.uri} style={{color: `white`, display: `inline-block`, marginRight: `1.5em`, ...props.style}} target="blank">
       <i style={{display: 'block', backgroundImage: `url("data:image/svg+xml;utf8,${props.icon}")`}} alt={props.title}></i>
       {props.title}
     </a>
