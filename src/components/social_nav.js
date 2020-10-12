78ed2df993dcf7f2a6bbc4c956b47ccd7406fe26
Andrew Park <atpark333@gmail.com>
2020-10-12 16:46:55 +0900

    Add instagram social nav icon and link

--- src/components/social_nav.js
+++ src/components/social_nav.js
@@ -4,6 +4,7 @@ import { css } from "astroturf";
 import GithubSvg from "../images/github.inline.svg";
 import TwitterSvg from "../images/twitter.inline.svg";
 import LinkedInSvg from "../images/linked_in.inline.svg";
+import InstagramSvg from "../images/instagram.svg";
 
 import IconLink from "./icon_link";
 
@@ -26,6 +27,7 @@ const SocialNav = () => {
       <IconLink uri="https://github.com/atparkweb" title="Github" iconImg={GithubSvg} />
       <IconLink uri="https://twitter.com/atparkweb2" title="Twitter" iconImg={TwitterSvg} />
       <IconLink uri="https://www.linkedin.com/in/andrew-park-13874719/" title="Linked In" iconImg={LinkedInSvg} />
+      <IconLink uri="https://www.instagram.com/pandabearscrewdriver/" title="Instagram" iconImg={InstagramSvg} />
     </nav>
   );
 };
