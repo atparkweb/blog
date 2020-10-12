fd5c6e9930eca4470d51084e9d971e1c589ad247
Andrew Park <atpark333@gmail.com>
2020-10-12 16:46:34 +0900

    Make profile image component

--- src/components/profile_image.js
+++ src/components/profile_image.js
@@ -13,10 +13,10 @@ import Img from "gatsby-image";
  * - `useStaticQuery`: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-const Image = () => {
+const ProfileImage = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      placeholderImage: file(relativePath: { eq: "me.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
@@ -33,4 +33,4 @@ const Image = () => {
   return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
 }
 
-export default Image;
+export default ProfileImage;
