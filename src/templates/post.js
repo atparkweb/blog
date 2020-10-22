802928d29d29a151be8483144fcea1b147558122
Andrew Park <atpark333@gmail.com>
2020-10-23 02:01:14 +0900

    Add default blog post template

---
+++ src/templates/post.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import Img from "gatsby-image"
+
+export default function Post({ data }) {
+  let post = data.markdownRemark
+  
+  let featuredImgFluid = post.frontmatter.featuredImage.childImageSharp.fluid
+  
+  return (
+    <Layout>
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+	<Img fluid={featuredImgFluid} />
+	<div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+query($slug: String!) {
+  markdownRemark(fields: { slug: { eq: $slug } }) {
+    html
+    frontmatter {
+      title
+      date(formatString: "DD MMMM, YYYY")
+      featuredImage {
+	childImageSharp {
+	  fluid(maxWidth: 800) {
+	    ...GatsbyImageSharpFluid
+	  }
+	}
+      }
+    }
+  }
+}
+`
