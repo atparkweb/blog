cf12f9f70809d893a70505141e1281364dae5cf6
Andrew Park <atpark333@gmail.com>
2020-10-27 19:39:30 +0900

    Make bio first page and move blog

---
+++ src/pages/blog.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { graphql } from "gatsby";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+export default function IndexPage({ data }) {
+  return (
+    <Layout>
+      <SEO title="Blog" />
+      <div style={{ maxWidth: `960px`, marginBottom: `1.45rem`, minHeight: `250px` }}>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h3>{node.frontmatter.title}{" "} <span>-- {node.frontmatter.date}</span></h3>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+query {
+  allMarkdownRemark {
+    totalCount
+    edges {
+      node {
+        id
+        frontmatter {
+          title
+          date(formatString: "DD MMMM, YYYY")
+          featuredImage {
+            childImageSharp {
+              fluid(maxWidth: 800) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        excerpt
+      }
+    }
+  }
+}
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,25 @@
 import React from "react";
-import { graphql } from "gatsby";
+import ProfileImage from "../components/profile_image";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-export default function IndexPage({ data }) {
-  console.log(data);
-  return (
-    <Layout>
-      <SEO title="Blog" />
-      <div style={{ maxWidth: `960px`, marginBottom: `1.45rem`, minHeight: `250px` }}>
-        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-        {data.allMarkdownRemark.edges.map(({ node }) => (
-          <div key={node.id}>
-            <h3>{node.frontmatter.title}{" "} <span>-- {node.frontmatter.date}</span></h3>
-            <p>{node.excerpt}</p>
-          </div>
-        ))}
+const IndexPage = () => (
+  <Layout>
+    <SEO title="About" />
+    <h2 className="page-title">About Me</h2>
+    <div style={{ maxWidth: `800px`, marginBottom: `1.45rem`, minHeight: `250px`, display: `flex` }}>
+      <div style={{ flex: `0 0 25%`, alignItems: `flex-start`, marginRight: `2rem` }}>
+	<ProfileImage />
       </div>
-    </Layout>
-  );
-};
+      <div style={{ flex: `0 0 75%`, alignItems: `flex-end` }}>
+        <p>I was born in Saint Paul, Minnesota, USA, and now living in Tokyo, Japan.
+        <br/>My interests include Music, Drawing, Photography, Sumo, and Sake.</p>
+        <p>I specialize in JavaScript programming, but I also have experience with other languages. I am currently building things with <a href="https://reactjs.org/" target="blank">React</a>, <a href="https://elixir-lang.org/" target="blank">Elixir</a>, and <a href="https://elm-lang.org/" target="blank">Elm</a>.</p>
+        <p>I am available to collaborate on web or mobile projects.<br />Please feel free to contact me.</p>
+      </div>
+    </div>
+  </Layout>
+);
 
-export const query = graphql`
-query {
-  allMarkdownRemark {
-    totalCount
-    edges {
-      node {
-        id
-        frontmatter {
-          title
-          date(formatString: "DD MMMM, YYYY")
-          featuredImage {
-            childImageSharp {
-              fluid(maxWidth: 800) {
-                ...GatsbyImageSharpFluid
-              }
-            }
-          }
-        }
-        excerpt
-      }
-    }
-  }
-}
-`
+export default IndexPage;
