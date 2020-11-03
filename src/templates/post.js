79dd91010ac360b021bc6b049d590485fa01d779
Andrew Park <atpark333@gmail.com>
2020-11-03 11:57:25 +0900

    Make featured image optional

--- src/templates/post.js
+++ src/templates/post.js
@@ -4,17 +4,19 @@ import Layout from "../components/layout"
 import Img from "gatsby-image"
 
 export default function Post({ data }) {
-  let post = data.markdownRemark
+  let post = data.markdownRemark;
   
-  let featuredImgFluid = post.frontmatter.featuredImage.childImageSharp.fluid
+  let featuredImgFluid = post.frontmatter.featuredImage ? post.frontmatter.featuredImage.childImageSharp.fluid: null;
   
   return (
     <Layout>
       <div>
         <h1>{post.frontmatter.title}</h1>
-	<div style={{width: `400px`, float: `right`, margin: `0 0 1em 1em`}}>
-	  <Img fluid={featuredImgFluid} />
-	</div>
+	{featuredImgFluid ? (
+	  <div style={{width: `400px`, float: `right`, margin: `0 0 1em 1em`}}>
+	    <Img fluid={featuredImgFluid} />
+	  </div>
+	) : ''}
 	<div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
     </Layout>
