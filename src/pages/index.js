f67070810fc9b39af4df3d0809a4022cae5766ff
Andrew Park <atpark333@gmail.com>
2021-12-24 13:14:25 +0900

    refactor: update about text

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,12 +10,12 @@ const IndexPage = () => (
     <h2 className="page-title">About Me</h2>
     <div style={{ maxWidth: `800px`, marginBottom: `1.45rem`, minHeight: `250px`, display: `flex` }}>
       <div style={{ flex: `0 0 25%`, alignItems: `flex-start`, marginRight: `2rem` }}>
-	<ProfileImage />
+        <ProfileImage />
       </div>
       <div style={{ flex: `0 0 75%`, alignItems: `flex-end` }}>
         <p>I was born in Saint Paul, Minnesota, USA, and now living in Tokyo, Japan.
         <br/>My interests include Music, Drawing, Photography, Sumo, and Sake.</p>
-        <p>I specialize in JavaScript programming, but I also have experience with other languages. I am currently building things with <a href="https://reactjs.org/" target="blank">React</a> and <a href="https://elixir-lang.org/" target="blank">Elixir</a>, and learning <a href="https://rust-lang.org" target="blank">another programming language</a>.</p>
+        <p>I specialize in JavaScript programming, but I also have experience with other languages. I am currently building things with <a href="https://reactjs.org/" target="blank">React</a> and <a href="https://elixir-lang.org/" target="blank">Elixir</a>.</p>
         <p>I am available to collaborate on web or mobile projects.<br />Please feel free to contact me.</p>
       </div>
     </div>
